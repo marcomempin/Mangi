d9aa8123d15903545e7d098511066b293ce18389
Marco Mempin <marcomempin@gmail.com>
2017-10-06 10:35:12 +0800

    Add SpinnerCell

---
+++ Mangi/Views/SpinnerCell.swift
@@ -0,0 +1,42 @@
+//
+//  SpinnerCell.swift
+//  Mangi
+//
+//  Created by Marco Mempin on 10/6/17.
+//  Copyright © 2017 marcowesome. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+func spinnerSectionController() -> ListSingleSectionController {
+    let configureBlock = { (item: Any, cell: UICollectionViewCell) in
+        guard let cell = cell as? SpinnerCell else { return }
+        cell.activityIndicator.startAnimating()
+    }
+    
+    let sizeBlock = { (item: Any, context: ListCollectionContext?) -> CGSize in
+        guard let context = context else { return .zero }
+        return CGSize(width: context.containerSize.width, height: 100)
+    }
+    
+    return ListSingleSectionController(cellClass: SpinnerCell.self,
+                                       configureBlock: configureBlock,
+                                       sizeBlock: sizeBlock)
+}
+
+class SpinnerCell: UICollectionViewCell {
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        self.contentView.addSubview(view)
+        return view
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let bounds = contentView.bounds
+        activityIndicator.center = CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+}
