1792e567a012f986eadc2341d537e0d3c9ad39c1
Marco Mempin <marcomempin@gmail.com>
2017-10-06 13:06:34 +0800

    Add LabelCell

---
+++ Mangi/Views/LabelCell.swift
@@ -0,0 +1,73 @@
+//
+//  LabelCell.swift
+//  Mangi
+//
+//  Created by Marco Mempin on 10/6/17.
+//  Copyright © 2017 marcowesome. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+func labelSectionController(with text: String) -> ListSingleSectionController {
+    let configureBlock = { (item: Any, cell: UICollectionViewCell) in
+        guard let cell = cell as? LabelCell else { return }
+        
+        cell.label.text = text
+    }
+    
+    let sizeBlock = { (item: Any, context: ListCollectionContext?) -> CGSize in
+        guard let context = context else { return .zero }
+        return CGSize(width: context.containerSize.width, height: 40)
+    }
+    
+    let sectionController = ListSingleSectionController(cellClass: LabelCell.self,
+                                                        configureBlock: configureBlock,
+                                                        sizeBlock: sizeBlock)
+    sectionController.selectionDelegate = sectionController.viewController as! ListSingleSectionControllerDelegate?
+    
+    return sectionController
+}
+
+class LabelCell: UICollectionViewCell {
+    
+    fileprivate static let insets = UIEdgeInsets(top: 8, left: 15, bottom: 8, right: 15)
+    fileprivate static let font = UIFont.systemFont(ofSize: 17)
+    
+    static var singleLineHeight: CGFloat {
+        return font.lineHeight + insets.top + insets.bottom
+    }
+    
+    static func textHeight(_ text: String, width: CGFloat) -> CGFloat {
+        let constrainedSize = CGSize(width: width - insets.left - insets.right, height: CGFloat.greatestFiniteMagnitude)
+        let attributes = [ NSFontAttributeName: font ]
+        let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+        let bounds = (text as NSString).boundingRect(with: constrainedSize, options: options, attributes: attributes, context: nil)
+        return ceil(bounds.height) + insets.top + insets.bottom
+    }
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.numberOfLines = 0
+        label.font = LabelCell.font
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(label)
+        contentView.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let bounds = contentView.bounds
+        label.frame = UIEdgeInsetsInsetRect(bounds, LabelCell.insets)
+    }
+    
+}
