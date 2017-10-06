5bedbd52476166ff2a1582144b01e13787635e92
Marco Mempin <marcomempin@gmail.com>
2017-10-06 14:05:20 +0800

    Add getMovieDetails function

--- Mangi/Systems/MovieLoader.swift
+++ Mangi/Systems/MovieLoader.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import Moya
+
+class MovieLoader {
+    
+    func getMovieDetails(for id: Int, completion: @escaping (Movie) -> Void) {
+        let provider = MoyaProvider<MangiAPI>()
+        provider.request(.movie(id: id)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let objectDictionary = try response.mapJSON() as! [String : Any]
+                    
+                    let movie = Movie.fromJSON(objectDictionary)
+                    
+                    completion(movie)
+                    
+                } catch {
+                    
+                }
+                
+            case let .failure(error):
+                guard let error = error as? CustomStringConvertible else {
+                    print("Cannot convert error to String")
+                    break
+                }
+                print(error.description)
+            }
+        }
+    }
+    
+}
