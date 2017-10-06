80ea330cd9976d2893152373cabba267ba83a389
Marco Mempin <marcomempin@gmail.com>
2017-10-06 09:59:28 +0800

    Add getMovies function

--- Mangi/Systems/DiscoverLoader.swift
+++ Mangi/Systems/DiscoverLoader.swift
@@ -11,4 +11,37 @@ import Moya
 
 class DiscoverLoader {
     
+    var movies: [Movie] = []
+    
+    func getMovies(for page: Int = 1, completion: @escaping () -> Void) {
+        let provider = MoyaProvider<MangiAPI>()
+        provider.request(.discover(page: page)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let objectDictionary = try response.mapJSON() as! [String : Any]
+                    
+                    let resultsArray = objectDictionary["results"] as! [[String : Any]]
+                    
+                    for movieJSON in resultsArray {
+                        let movie = Movie.fromJSON(movieJSON)
+                        self.movies.append(movie)
+                    }
+                    
+                    completion()
+                    
+                } catch {
+                    
+                }
+                
+            case let .failure(error):
+                guard let error = error as? CustomStringConvertible else {
+                    print("Cannot convert error to String")
+                    break
+                }
+                print(error.description)
+            }
+        }
+    }
+    
 }
