27f706307bb1bcf12d8652965a606912f6877b5a
Marco Mempin <marcomempin@gmail.com>
2017-10-06 09:11:16 +0800

    Add DiscoverLoader

---
+++ Mangi/Systems/DiscoverLoader.swift
@@ -0,0 +1,14 @@
+//
+//  DiscoverLoader.swift
+//  Mangi
+//
+//  Created by Marco Mempin on 10/6/17.
+//  Copyright © 2017 marcowesome. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+class DiscoverLoader {
+    
+}
